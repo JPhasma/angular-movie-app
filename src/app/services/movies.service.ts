b5da285f26d2f7291e718154e8b1688038af5782
JaneDalton <26035724+JPhasma@users.noreply.github.com>
2023-01-21 13:50:19 +0000

    setup movies service to return data from api to app

--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MoviesService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getMovies() {
+    return this.http.get(
+      'https://api.themoviedb.org/3/movie/upcoming?api_key=97f4a4a0dd6cc5bc8673df3268e4f042'
+    );
+  }
 }
