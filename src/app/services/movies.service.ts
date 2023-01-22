454b55ae7b2f41a4f3a10f64bcf0f7692767e4d5
JaneDalton <26035724+JPhasma@users.noreply.github.com>
2023-01-22 19:16:11 +0000

    used rxjs to control api data returned

--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { MovieDto } from '../models/movie';
+import { switchMap } from 'rxjs/operators';
+import { of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -11,9 +13,13 @@ export class MoviesService {
 
   constructor(private http: HttpClient) {}
 
-  getMovies(type: string = 'upcoming') {
-    return this.http.get<MovieDto>(
-      `${this.baseUrl}/movie/${type}?api_key=${this.apiKey}`
-    );
+  getMovies(type: string = 'upcoming', count: number = 6) {
+    return this.http
+      .get<MovieDto>(`${this.baseUrl}/movie/${type}?api_key=${this.apiKey}`)
+      .pipe(
+        switchMap((res) => {
+          return of(res.results.slice(0, count));
+        })
+      );
   }
 }
